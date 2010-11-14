3ee892881a5933cdd748c67c36d39c159a15a268
ecavazos <ejcavazos@gmail.com>
2010-11-13 22:37:17 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackalack::Application.config.secret_token = '66987ffe5823ba2a37eb255de40270814f2d4a04a144d13fdb0a8b218a21110b6e00eb23bec28b0bd5b3556279dfa89d3fdca83e932585da390b3d2e10c83ee0'
