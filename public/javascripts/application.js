4fd5bfa565ed0400c75753af8bd9ab1e4321cdf3
ecavazos <ejcavazos@gmail.com>
2010-11-20 20:16:04 -0800

    brainstorming app layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function () {
+  /*
+  jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", ( $(window).height() - this.outerHeight() ) / 2 + $(window).scrollTop() + "px");
+    this.css("left", ( $(window).width() - this.outerWidth() ) / 2 + $(window).scrollLeft() + "px");
+    return this;
+  };
+  */
+
+  $('a').click(function () {
+    var dialogHtml = '<div id="dialog" title="dialog"><input type="text" /><input type="submit" value="WTF" /></div>';
+
+    var opts = {
+      closeText: 'x',
+      position: 'center'
+    };
+
+    $(dialogHtml).dialog(opts);
+  });
+});
