cf40e403dd715d4c2d23bd5c30e2dec1ca9b03d2
ecavazos <ejcavazos@gmail.com>
2010-11-28 21:21:17 -0800

    working on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,14 @@ class HomeController < ApplicationController
     @time_entries = TimeEntry.
                       includes(:user, :project => :client).
                       order('created_at desc')
+
+    @recent_projects = Project.joins(:time_entries).
+                        select('distinct(projects.name)').
+                        order('time_entries.updated_at').
+                        limit(10)
+
+    @clients = Client.order('created_at desc').limit(10)
+    @projects = Project.order('created_at desc').limit(10)
   end
 
 end
