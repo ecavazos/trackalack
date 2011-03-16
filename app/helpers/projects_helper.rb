3079606deb609543e4c65b15e07331b6937e7d05
ecavazos <ejcavazos@gmail.com>
2011-03-15 18:49:03 -0700

    helper methods to secure edit and delete links

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  def secure_edit(time_entry)
+    return "" if current_user.id != time_entry.user.id
+    return link_to 'Edit', edit_project_time_entry_path(time_entry.project, time_entry)
+  end
+
+  def secure_destroy(time_entry)
+    return "" if current_user.id != time_entry.user.id
+    return link_to 'Destroy', project_time_entry_path(time_entry.project, time_entry), :confirm => 'Are you sure?', :method => :delete
+  end
 end
