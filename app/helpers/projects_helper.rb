af3e109b5b2eff2df4f00f6eace7d303a0745783
ecavazos <ejcavazos@gmail.com>
2011-03-15 20:06:13 -0700

    DRYer code

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,15 @@
 module ProjectsHelper
   def secure_edit(time_entry)
-    return "" if current_user.id != time_entry.user.id
+    return "" if not_owned_by_user?(time_entry)
     return link_to 'Edit', edit_project_time_entry_path(time_entry.project, time_entry)
   end
 
   def secure_destroy(time_entry)
-    return "" if current_user.id != time_entry.user.id
+    return "" if not_owned_by_user?(time_entry)
     return link_to 'Destroy', project_time_entry_path(time_entry.project, time_entry), :confirm => 'Are you sure?', :method => :delete
+  end
+
+  def not_owned_by_user?(time_entry)
+    return current_user.id != time_entry.user.id
   end
 end
