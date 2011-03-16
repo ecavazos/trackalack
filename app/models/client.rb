ba316d8cea12da26958996386c4139611bf9fe7a
ecavazos <ejcavazos@gmail.com>
2011-03-15 21:26:01 -0700

    customer short name

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,4 +4,17 @@ class Client < ActiveRecord::Base
   validates_presence_of :name
 
   scope :recently_created, lambda { |qty| order('created_at desc').limit(qty) }
+
+  def short_name
+    split = self.name.split(' ')
+
+    a = split.shift
+    b = ''
+
+    split.each do |x|
+      b = b + x[0]
+    end
+
+    return "#{a} #{b}."
+  end
 end
