d20538ada8ea55a0f35cc50e4edbc92d56f31001
ecavazos <ejcavazos@gmail.com>
2011-03-16 20:08:36 -0700

    timeline experiments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @time_entries = current_user.time_entries.
                       includes(:user, :project => :client).
-                      order('created_at desc').limit(25)
+                      order('created_at desc').limit(50)
 
     @recently_updated_projects = Project.recently_updated(10)
     @clients = Client.recently_created(10)
