ef362c0b18fc6540df7bd4c558a6f46d832f71e8
ecavazos <ejcavazos@gmail.com>
2011-04-10 08:32:18 -0700

    home page shows everyones recent data

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -6,6 +6,7 @@ module HomeHelper
 
   def user_edit_time_link(time_entry)
     return unless time_entry.user == current_user
+    haml_concat('&middot;'.html_safe)
     haml_tag('.activity-meta-item') do
       haml_concat link_to 'Edit', edit_project_time_entry_path(time_entry.project, time_entry),
         :class => 'edit-time-link'
