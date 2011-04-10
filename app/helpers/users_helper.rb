05d6fed219cadde5a4490cfff4f4ec55236e3b48
ecavazos <ejcavazos@gmail.com>
2011-04-10 11:38:21 -0700

    styles for alerts, notices and errors. bug fix in title helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,14 +6,16 @@ module UsersHelper
   def user_show_time_title
     base = 'Time entries'
 
-    if params[:days]
-      return "#{base} for last #{params[:days]} days"
-    elsif flash[:error] || params[:start_date].nil?
-      return base
+    if no_filter_params?
+      return "#{base} for last 5 days"
     elsif !params[:start_date].empty? && !params[:end_date].empty?
       return "#{base} between #{params[:start_date]} and #{params[:end_date]}"
     elsif !params[:start_date].empty? && params[:end_date].empty?
       return "#{base} for #{params[:start_date]}"
+    elsif params[:days]
+      return "#{base} for last #{params[:days]} days"
+    elsif flash[:error] || params[:start_date].nil?
+      return base
     end
   end
 
@@ -29,4 +31,11 @@ module UsersHelper
   def destroy_link_data(time)
     {:name => 'Destroy', :path => project_time_entry_path(time.project, time)}
   end
+
+  private
+
+  def no_filter_params?
+    !(params[:days] || params[:start_date] || params[:end_date])
+  end
+
 end
