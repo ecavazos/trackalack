48a19d57ab522e4421cbddc3e3f1f2a1e39e3bd1
ecavazos <ejcavazos@gmail.com>
2011-05-06 18:19:28 -0700

    added specs for client_helper and home_helper

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,15 +1,55 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the HomeHelper. For example:
-#
-# describe HomeHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe HomeHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#format_date(date)" do
+    describe "when given a valid date" do
+      it "should format date as mm/dd/yyyy" do
+        helper.format_date(Date.new(2011, 5, 5)).should eq('05/05/2011')
+      end
+    end
+
+    describe "when date is nil" do
+      it "should be blank" do
+        helper.format_date(nil).blank?.should be_true
+      end
+    end
+  end
+
+  describe "#user_edit_time_link(time_entry)" do
+    describe "when current_user owns time_entry" do
+      before do
+        helper.extend Haml::Helpers
+        helper.init_haml_helpers
+        
+        @user = create_user
+        @time_entry = mock_model(TimeEntry).as_null_object
+        @time_entry.stub(:user).and_return(@user)
+        helper.stub(:current_user).and_return(@user)
+      end
+
+      it "should contain link to edit time" do
+        id = @time_entry.id
+        helper.capture_haml {
+          helper.user_edit_time_link(@time_entry)
+        }.should match(/<a href="\/projects\/#{id}\/time_entries\/#{id}\/edit\" class="edit-time-link">Edit<\/a>/)
+      end
+
+      it "should contain active-meta-item class" do
+        helper.capture_haml {
+          helper.user_edit_time_link(@time_entry)
+        }.should match(/class='activity-meta-item'/)
+      end
+    end
+
+    describe "when current_user is not the owner of the time_entry" do
+      before do
+        @time_entry = mock_model(TimeEntry).as_null_object
+        helper.stub(:current_user).and_return(nil)
+      end
+
+      it "should be nil" do
+        helper.user_edit_time_link(@time_entry).should be_nil
+      end
+    end
+  end
 end
