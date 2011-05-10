8db7386448aa4c17caa348d07d4bbf59eb37962e
ecavazos <ejcavazos@gmail.com>
2011-05-09 20:18:07 -0700

    search index spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @project = Project.new :name => 'foo'
+  end
+
+  describe "validations" do
+    it "is not valid without name" do
+      @project.name = ""
+      @project.should_not be_valid
+    end
+  end
 end
--- spec/models/search_index_spec.rb
+++ spec/models/search_index_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe SearchIndex do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#to_path_method" do
+    it "should append '_path' to the resource type" do
+      si = SearchIndex.new :resource_type => 'Client'
+      si.to_path_method.should eq('client_path')
+    end
+  end
 end
