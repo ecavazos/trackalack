22e3979c27a8978b02b7ca829cb524307c2b3629
ecavazos <ejcavazos@gmail.com>
2011-05-11 19:03:54 -0700

    user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = create_user(:first_name => 'Foo', :last_name => 'Bar')
+  end
+
+  describe "#fullname" do
+    subject { @user.fullname }
+
+    it { should eq('Foo Bar') }
+  end
+
+  describe "#pathname" do
+    subject { @user.pathname }
+
+    it { should eq('foo_bar') }
+  end
 end
--- spec/support/fixjour_builders.rb
+++ spec/support/fixjour_builders.rb
@@ -3,6 +3,6 @@ require 'fixjour'
 Fixjour do
   define_builder(User, :first_name => 'foo', :last_name => 'bar', :email => "testuser@trackalack.com", :password => '1234')
   define_builder(SearchIndex, :resource_id => '37', :name => 'foo', :resource_type => 'client')
-  define_builder(TimeEntry, :date => Date.new, :duration => 3.5, :work_type => :feature, :billing_type => :billable, :description => 'foo')
+  define_builder(TimeEntry, :date => Date.today, :duration => 3.5, :work_type => :feature, :billing_type => :billable, :description => 'foo')
 end
 
