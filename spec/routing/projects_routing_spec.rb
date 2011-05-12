9840f90a1a73b7b0e1bfceff3ba7806f620e46e0
ecavazos <ejcavazos@gmail.com>
2011-05-11 20:41:53 -0700

    making routing specs pass

--- spec/routing/projects_routing_spec.rb
+++ spec/routing/projects_routing_spec.rb
@@ -7,20 +7,12 @@ describe ProjectsController do
       { :get => "/projects" }.should route_to(:controller => "projects", :action => "index")
     end
 
-    it "recognizes and generates #new" do
-      { :get => "/projects/new" }.should route_to(:controller => "projects", :action => "new")
-    end
-
     it "recognizes and generates #show" do
       { :get => "/projects/1" }.should route_to(:controller => "projects", :action => "show", :id => "1")
     end
 
     it "recognizes and generates #edit" do
       { :get => "/projects/1/edit" }.should route_to(:controller => "projects", :action => "edit", :id => "1")
-    end
-
-    it "recognizes and generates #create" do
-      { :post => "/projects" }.should route_to(:controller => "projects", :action => "create")
     end
 
     it "recognizes and generates #update" do
