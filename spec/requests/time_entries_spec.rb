ae408388a30b95d92140008c6cc221ad5866eb8c
ecavazos <ejcavazos@gmail.com>
2011-05-11 20:44:23 -0700

    removed wothless request specs

--- spec/requests/time_entries_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "TimeEntries" do
-  describe "GET /time_entries" do
-    it "works! (now write some real specs)" do
-      get time_entries_path
-    end
-  end
-end
