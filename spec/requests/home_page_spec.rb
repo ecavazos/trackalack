1205c6d6ee5444ca2e61c04f3d91214bcc862b66
ecavazos <ejcavazos@gmail.com>
2011-07-11 09:34:17 -0700

    more home page specs

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -80,9 +80,51 @@ describe 'Home Page', :js => true do
         end
       end
 
+      it 'allows user to add time entries to associated project' do
+        visit root_path
+
+        page.should have_no_content 'Time Entry'
+
+        within '#stream .activity' do
+          click_link 'Add time'
+        end
+
+        within '.ui-dialog' do
+          page.should have_content 'Time Entry'
+
+          page.execute_script("$('#time_entry_work_type option:eq(1)').attr('selected', 'selected');")
+          page.execute_script("$('#time_entry_billing_type option:eq(2)').attr('selected', 'selected');")
+
+          find('label', :text => 'Date').click
+          fill_in 'Date', :with => Date.today.strftime('%m/%d/%Y')
+        end
+
+        find('#ui-datepicker-div .ui-state-highlight').click
+
+        within '.ui-dialog' do
+          fill_in 'Duration',    :with => '2.5'
+          fill_in 'Description', :with => 'I had a nice time with your mom.'
+          click_button 'Save'
+        end
+
+        time_entry = TimeEntry.first
+        time_entry.project.should      == @project
+        time_entry.work_type.should    == :task
+        time_entry.billing_type.should == :no_charge
+        time_entry.date.should         == Date.today
+        time_entry.duration.should     == 2.5
+        time_entry.description.should  == 'I had a nice time with your mom.'
+
+        within '#stream .activity' do
+          page.should have_content 'I had a nice time with your mom.'
+        end
+      end
+
       it 'allows owner to edit' do
 
         visit root_path
+
+        page.should have_no_content 'Time Entry'
 
         within '#stream .activity' do
           click_link 'Edit'
@@ -90,14 +132,14 @@ describe 'Home Page', :js => true do
 
         within '.ui-dialog' do
           page.should have_content 'Time Entry'
-          fill_in 'time_entry[description]', :with => 'Make that super-awesomeness!'
+          fill_in 'Description', :with => 'Make that super-awesomeness!'
           click_button 'Save'
         end
 
-        time_entry.reload
+        @time_entry.reload
         @time_entry.description.should == 'Make that super-awesomeness!'
 
-        # TODO: page should refresh the updated time entry
+        # TODO: page should refresh the updated time entry *ONLY*
       end
 
       it "hides edit for other user's time entries" do
